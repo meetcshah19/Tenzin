abfc6d2c95787fc52e485e258076527da6b23da2
Meet Shah <meet@Meets-MacBook-Pro.local>
2020-01-15 21:27:41 +0530

    changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
 exports.home=(req,res,next)=>{
-    res.sendFile('/Users/meet/Desktop/Tenzin/views/index.html');
+    res.sendFile(__dirname.replace('controllers',"views/index.html"));
+    // res.sendFile('/Users/meet/Desktop/Tenzin/views/index.html');
 };
